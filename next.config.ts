5c087dfaaa7a827e86da27b997403cb556e6b686
gautchap <gautchap+github@outlook.com>
2024-12-27 11:55:44 +0100

    Integrate next-intl plugin and update Next.js configuration for experimental React compiler

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    experimental: {
+        reactCompiler: process.env.NODE_ENV === "production" ? true : undefined,
+    },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
